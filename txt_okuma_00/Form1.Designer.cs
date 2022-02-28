fde1dcb3be90f713194f62db9b678cf6a5beb498
trkey1923 <trkey1923@gmail.com>
2022-02-28 11:20:24 +0300

    Proje dosyası ekle.

---
+++ txt_okuma_00/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace txt_okuma_00
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYol = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHepsiSec = new System.Windows.Forms.Button();
+            this.btnDigerSec = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnYol
+            // 
+            this.btnYol.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYol.Location = new System.Drawing.Point(558, 23);
+            this.btnYol.Name = "btnYol";
+            this.btnYol.Size = new System.Drawing.Size(84, 43);
+            this.btnYol.TabIndex = 0;
+            this.btnYol.Text = "Yol";
+            this.btnYol.UseVisualStyleBackColor = true;
+            this.btnYol.Click += new System.EventHandler(this.btnYol_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(50, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 43);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.CheckBoxes = true;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(50, 72);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1235, 366);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(990, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dosya Sayısı :";
+            // 
+            // btnHepsiSec
+            // 
+            this.btnHepsiSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHepsiSec.Location = new System.Drawing.Point(643, 23);
+            this.btnHepsiSec.Name = "btnHepsiSec";
+            this.btnHepsiSec.Size = new System.Drawing.Size(84, 43);
+            this.btnHepsiSec.TabIndex = 4;
+            this.btnHepsiSec.Text = "Hepsini Seç";
+            this.btnHepsiSec.UseVisualStyleBackColor = true;
+            this.btnHepsiSec.Click += new System.EventHandler(this.btnHepsiSec_Click);
+            // 
+            // btnDigerSec
+            // 
+            this.btnDigerSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDigerSec.Location = new System.Drawing.Point(728, 23);
+            this.btnDigerSec.Name = "btnDigerSec";
+            this.btnDigerSec.Size = new System.Drawing.Size(84, 43);
+            this.btnDigerSec.TabIndex = 5;
+            this.btnDigerSec.Text = "Diğerlerini Seç";
+            this.btnDigerSec.UseVisualStyleBackColor = true;
+            this.btnDigerSec.Click += new System.EventHandler(this.btnDigerSec_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Red;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(813, 23);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(84, 43);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 450);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnDigerSec);
+            this.Controls.Add(this.btnHepsiSec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnYol);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYol;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHepsiSec;
+        private System.Windows.Forms.Button btnDigerSec;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
---
+++ txt_okuma_00/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace txt_okuma_00
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Sayac=0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("Adı", 150);
+            listView1.Columns.Add("Yolu", 300);
+            listView1.Columns.Add("Boyut", 50);
+            listView1.Columns.Add("İçerik", 1200);
+
+        }
+
+        private void btnYol_Click(object sender, EventArgs e)
+
+        {
+            Sayac = 0;
+            
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+            ListViewDoldur(textBox1.Text);
+        }
+
+        private void ListViewDoldur(String yol)
+        {
+            listView1.Items.Clear();
+            Directory.CreateDirectory(yol);
+            string[] yoldakiDosyalar = Directory.GetFiles(yol, "*.txt");
+            if (yoldakiDosyalar.Length < 100)
+            {
+                label1.Text = "Dosya Sayısı: " + yoldakiDosyalar.Length.ToString();
+                Sayac = yoldakiDosyalar.Length;
+            }
+            else
+            {
+                label1.Text = "Dosya Sayısı 0-70 / " + yoldakiDosyalar.Length.ToString();
+                Sayac=70;
+                label1.ForeColor = Color.Red;
+            }
+            for (int i = 0; i < Sayac; i++)
+            {
+                string[] DosyaBilgileri = DosyaBilgileriniAl(yoldakiDosyalar[i]);
+                var satır = new ListViewItem(DosyaBilgileri);
+                listView1.Items.Add(satır);
+            }
+        }
+
+        private String[] DosyaBilgileriniAl(string v)
+        {
+            String[] DosyaBilgileri = new String[4];
+            string yol = v;
+            FileInfo fi = new FileInfo(yol);
+            long dosyaBoyutu = fi.Length;
+            String dosyaAdı = fi.Name;
+            String icerik = DosyaOku(v);
+            DosyaBilgileri[0] = dosyaAdı;
+            DosyaBilgileri[1] = yol;
+            DosyaBilgileri[2] = dosyaBoyutu.ToString();
+            DosyaBilgileri[3] = icerik;
+
+            return DosyaBilgileri;
+        }
+
+        private string DosyaOku(string v)
+        {
+            StreamReader streamReader = new StreamReader(v);
+            char[] karakterler = new char[500];
+            streamReader.ReadBlock(karakterler, 0, 500);
+            streamReader.Close();
+            String Metin = new String(karakterler);
+            return Metin;
+        }
+
+        private void btnHepsiSec_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem satır in listView1.Items)
+            {
+                satır.Checked = true;
+            }
+        }
+
+        private void btnDigerSec_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem satır in listView1.Items)
+            {
+                if (satır.Checked == true)
+                {
+                    satır.Checked = false;
+                }
+                else
+                    satır.Checked = true;
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+                    foreach (ListViewItem satır in listView1.Items)
+                {
+                    if (satır.Checked==true)
+                    {
+                        //MessageBox.Show(satır.SubItems[1].Text);
+                        string yol=satır.SubItems[1].Text;
+                        File.Delete(yol);
+
+                    }
+                }
+            
+            ListViewDoldur(textBox1.Text);
+        }
+    }
+}
